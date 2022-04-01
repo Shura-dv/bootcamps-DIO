0d937faa856dafab2dfec9b04c8b8fa7bfc3c8a2
Alyson Figueiredo <102189969+Shura-dv@users.noreply.github.com>
2022-04-01 06:59:49 -0300

    Delete curso-trabalhando-com-modulos directory

--- curso-trabalhando-com-modulos/README.md
+++
@@ -1,1 +0,0 @@
-Este repositório contém a atividade prática do Curso "Módulos" aplicado pela Engenheira de Software Stephany Nusch, da [Digital Innovation One](https://digitalinnovation.one/).
--- curso-trabalhando-com-modulos/main.mjs
+++
@@ -1,5 +0,0 @@
-import { mostraIdade, mostraCidade, mostraHobby }  from "./funcoes.mjs";
-
-console.log(mostraIdade("Pedro", 20));
-console.log(mostraCidade("Pedro", "Curitiba"));
-console.log(mostraHobby("Pedro", "programar"));
